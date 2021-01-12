377f8acfe9cdbc67936f4ccdff70ba11cd558f7a
Brian High <high@uw.edu>
2021-01-12 15:10:14 -0800

    Updated scripts for BWT website changes.

--- R/bwt.R
+++ R/bwt.R
@@ -1,56 +1,51 @@
 # bwt.R: Extract border wait time data from XML using R.
 #
-# Tested on Ubuntu 14.04.3 LTS and R version 3.2.3 (2016-06-06).
+# Tested on Ubuntu 18.04.5 LTS and R version 3.6.3 (2020-02-29).
 #
 # Schedule to run once an hour, at half past the hour, e.g.:
 #     $ echo '30 * * * * Rscript ~/bin/bwt.R' | crontab
 
-url <- "http://apps.cbp.gov/bwt/bwt.xml"
+# Define variables
+url <- "https://bwt.cbp.gov/xml/bwt.xml"
 loc <- "San Ysidro"
-csv <- "data/syR.csv"
 
 # Create data folder
-dir.create(file.path('data'), showWarnings=FALSE, recursive=TRUE)
+data_dir <- 'data'
+dir.create(file.path(data_dir), showWarnings=FALSE, recursive=TRUE)
 
-#install.packages("XML")
-library(XML)
+# Load packages, installing as needed
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(xml2, dplyr, purrr)
 
 # Parse XML
-xmlfile <- xmlTreeParse(url)
-xmltop = xmlRoot(xmlfile)
+xml <- read_xml(url)
+
+# Get ports
+ports_lst <- as_list(xml_find_all(xml, "/border_wait_time/port"))
+names(ports_lst) <- sapply(ports_lst, function(x) x$port_number)
+ports <- ports_lst %>% 
+  map(.x = ., .f = ~as_tibble(flatten(.x[1:10]))) %>%
+  bind_rows(.id = "port_number")
+
+# Get lane data
+lane_types <- c("passenger_vehicle_lanes", "commercial_vehicle_lanes")
+lane_type <- rep(lane_types, each = length(ports_lst))
+lane_data <- map2(.x = rep(ports_lst, 2),
+     .y = lane_type, 
+     .f = ~as_tibble(flatten(.x[[.y]]$standard_lanes), .y = .y)) %>%
+  bind_rows(.id = "port_number") %>%
+  mutate(lane_type = lane_type)
 
 # Find port of entry
-ports <- sapply(getNodeSet(xmltop, "/border_wait_time/port/port_name"), 
-                xmlValue)
-ns <- getNodeSet(xmltop, '/border_wait_time/port')
-loc.ns <- ns[[grep(loc, ports)]]
+port_num <- ports %>% 
+  filter(port_name == loc) %>%
+  pull(port_number)
 
-# We can't just run xmlValue on the whole XML node, as the cells run together
-#loc.v <- as.vector(unlist(xmlSApply(loc.ns, xmlValue)))
-#loc.v[8]
-#[1] "25At 1:00 pm PSTdelay6010At 1:00 pm PSTdelay108At 1:00 pm PSTdelay407"
+# Filter data for port of entry
+port_data <- inner_join(ports %>% filter(port_number %in% port_num),
+                        lane_data %>% filter(port_number %in% port_num),
+                        by = "port_number")
 
-# Store top-level node attributues as a vector
-loc.top.v <- as.vector(unlist(xmlSApply(loc.ns, xmlValue)))[c(1:6)]
-
-# Store child node attributues as a vector
-loc.children.v <- vector()
-for (i in c("commercial_vehicle_lanes", 
-            "passenger_vehicle_lanes", 
-            "pedestrian_lanes")) {
-    for (j in names(xmlSApply(loc.ns[[i]], xmlValue))) {
-        loc.children.v <- c(loc.children.v, 
-                            xmlSApply(loc.ns[[i]][[j]], xmlValue))
-    }
-}
-
-# Combine vectors with a timestamp
-loc.v <- c(format(Sys.time(), "%Y-%m-%d %H:%M:%S"), loc.top.v, loc.children.v)
-
-# Remove values of "character(0)"
-loc.v <- gsub("character(0)", "", loc.v, fixed = TRUE)
-
-# Append to CSV
-loc.m <- as.matrix(t(loc.v))
-write.table(loc.m, file = csv, sep = ",", row.names = FALSE,
-            col.names = FALSE, append=TRUE)
+# Write output to csv
+csv <- file.path(data_dir, "syR.csv")
+write.csv(port_data, csv, row.names = FALSE)
